bd29a7f62273eecbfe998c274f0e920414a678d5
CodeGhost237 <arielbanaken@gmail.com>
2024-10-06 05:56:27 +0100

    Erreur au niveau de la barre de recherche

--- Searchapp/src/components/forms/checkbox.jsx
+++ Searchapp/src/components/forms/checkbox.jsx
@@ -1,14 +1,17 @@
 export function Checkbox({onChange, label, checked, id}){
 
+    const style = {cursor:"pointer"}
+
     return (
         <div className="form-check">
             <input type="checkbox"
+            style={style}
             id={id}
             className="form-check-input"
             checked = {checked}
             onChange={e => onChange(e.target.checked)}
             />
-            <label className="form-checked-label" htmlFor={id}>{label}</label>
+            <label className="form-checked-label" style={style} htmlFor={id}>{label}</label>
         </div>
     )
 }
